435e042f7a19943cde60d288a656c45b1d6a1c93
LucasGandel <lucas.gandel@gmail.com>
2017-07-22 14:27:36 +0200

    First commit
    Copy project from git@github.com:Modelisation5ETI/TP5_Skinning.git

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# ProceduralMap
-Prodedural generation of maps
+##*Procedural map generation*
+
+-----
+**CMake Build instructions**
+
+-----
+  - Out-of-source build :
+ ```
+git clone git@github.com:LucasGandel/ProceduralMap.git && \
+mkdir ProceduralMap-build && cd ProceduralMap-build  && \
+cmake ../ProceduralMap  && \
+make -j
+ 
+ ```
+  - QtCreator :
+ ```
+git clone git@github.com:LucasGandel/ProceduralMap.git  && \
+mkdir ProceduralMap-build && cd ProceduralMap-build  && \
+qtcreator ../ProceduralMap
+ 
+ ```
+  *Then configure project using QtCreator API* : 
+   - *Set the build directory to ProceduralMap-build/*
+   - *Run CMake and build project*
+
---
+++ src/skinning/skeleton_joint.cpp
@@ -0,0 +1,39 @@
+/*
+**    TP CPE Lyon
+**    Copyright (C) 2015 Damien Rohmer
+**
+**    This program is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**   This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "skeleton_joint.hpp"
+
+namespace cpe
+{
+
+skeleton_joint::skeleton_joint()
+    :position(),orientation()
+{}
+skeleton_joint::skeleton_joint(vec3 const& position_param,quaternion const& orientation_param)
+    :position(position_param),orientation(orientation_param)
+{}
+
+mat4 skeleton_joint::to_mat4()
+{
+  mat4 transform;
+  transform.set_transformation( orientation.to_mat3(), position );
+  return transform;
+}
+
+}
