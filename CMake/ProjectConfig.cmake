a54075419f54c522e32b8d7310b88ac15ba6eac9
LucasGandel <lucas.gandel@gmail.com>
2017-08-19 11:41:51 +0200

    STYLE: Rename Interface folder to Application
    Improve consistency.

--- CMake/ProjectConfig.cmake
+++ CMake/ProjectConfig.cmake
@@ -2,7 +2,7 @@
 set( GLTK_SRC_DIRS
   "${SRC_DIR}/Core"
   "${SRC_DIR}/DebugHelpers"
-  "${SRC_DIR}/Interface"
+  "${SRC_DIR}/Application"
   "${SRC_DIR}/Math"
   "${SRC_DIR}/Math/3d"
   "${SRC_DIR}/Math/Perlin"
